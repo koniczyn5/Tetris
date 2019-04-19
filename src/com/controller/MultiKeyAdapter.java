a22dd050798bfd5aeefa18c22bc54a34c110ce90
konic <koniczyn5@gmail.com>
2019-04-19 18:48:45 +0200

    Second version of detecting multiple keys pressed. Added game restarting.

--- src/com/controller/MultiKeyAdapter.java
+++ src/com/controller/MultiKeyAdapter.java
@@ -5,22 +5,24 @@ import com.model.Shape;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.TreeSet;
 
+//TODO: Fix the bug with sequence: Press A, Press D, Release D
 public class MultiKeyAdapter extends KeyAdapter {
 
-    private boolean[] Keys;
+    private TreeSet<Integer> Keys;
     private Board board;
     MultiKeyAdapter(Board board)
     {
         this.board=board;
-        Keys= new boolean[KeyEvent.VK_Z];
+        Keys= new TreeSet<>();
     }
 
     @Override
     public synchronized void keyPressed(KeyEvent e) {
         System.out.println("key pressed");
         int keyCode = e.getKeyCode();
-        Keys[keyCode]=true;
+        Keys.add(keyCode);
         doKeysActions();
     }
 
@@ -29,47 +31,47 @@ public class MultiKeyAdapter extends KeyAdapter {
     {
         System.out.println("key released");
         int keyCode = e.getKeyCode();
-        Keys[keyCode]=false;
+        Keys.remove(keyCode);
     }
 
     private void doKeysActions()
     {
-        if(board.isPaused() || board.getCurPiece().getShape() == Shape.Tetrominoe.NoShape){return;}
+        if(Keys.isEmpty() || board.isPaused() || board.getCurPiece().getShape() == Shape.Tetrominoe.NoShape){return;}
         if (!board.isStarted()) {
-            if(Keys[KeyEvent.VK_R])
+            if(Keys.contains(KeyEvent.VK_R))
             {
                 board.start();
             }
             return;
         }
 
-        if(Keys[KeyEvent.VK_ESCAPE])
+        if(Keys.contains(KeyEvent.VK_ESCAPE))
         {
             board.pause();
             return;
         }
 
-        if(Keys[KeyEvent.VK_A] && !Keys[KeyEvent.VK_D])
+        if(Keys.contains(KeyEvent.VK_A) && !Keys.contains(KeyEvent.VK_D))
         {
             board.tryMove(board.getCurPiece(), board.getCurX() - 1, board.getCurY());
         }
-        if(Keys[KeyEvent.VK_D] && !Keys[KeyEvent.VK_A])
+        if(Keys.contains(KeyEvent.VK_D) && !Keys.contains(KeyEvent.VK_A))
         {
             board.tryMove(board.getCurPiece(), board.getCurX() + 1, board.getCurY());
         }
-        if(Keys[KeyEvent.VK_Q] && !Keys[KeyEvent.VK_E])
+        if(Keys.contains(KeyEvent.VK_Q) && !Keys.contains(KeyEvent.VK_E))
         {
             board.tryMove(board.getCurPiece().rotateLeft(), board.getCurX(), board.getCurY());
         }
-        if(Keys[KeyEvent.VK_E] && !Keys[KeyEvent.VK_Q])
+        if(Keys.contains(KeyEvent.VK_E) && !Keys.contains(KeyEvent.VK_Q))
         {
             board.tryMove(board.getCurPiece().rotateRight(), board.getCurX(), board.getCurY());
         }
-        if(Keys[KeyEvent.VK_S])
+        if(Keys.contains(KeyEvent.VK_S))
         {
             board.oneLineDown();
         }
-        if(Keys[KeyEvent.VK_SPACE])
+        if(Keys.contains(KeyEvent.VK_SPACE))
         {
             board.dropDown();
         }
